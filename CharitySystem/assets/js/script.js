5c8071fe3e94c9827ca2fa99dc856559284f2bc3
asmajamil006-source <asmajamil006@gmail.com>
2026-08-30 16:21:05 +0500

    Delete CharitySystem/assets/js/script.js

--- CharitySystem/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function toggleOther() {
-    let category = document.getElementById("category").value;
-    let field = document.getElementById("otherField");
-
-    if (field) {
-        field.style.display = (category === "Other") ? "block" : "none";
-    }
-}
